a3db4d04f3e526db26c57365979f67739f158ca8
Ralf Schmitt <ralf@systemexit.de>
2013-06-11 15:01:42 +0200

    import lein app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fscrawler-tika-convert
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/fscrawler_tika_convert/core.clj
@@ -0,0 +1,9 @@
+(ns fscrawler-tika-convert.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+  (println "Hello, World!"))
