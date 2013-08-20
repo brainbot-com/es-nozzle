df114c4f0db644c777d1046e569cb093d453cc33
Ralf Schmitt <ralf@systemexit.de>
2013-08-20 11:22:14 +0200

    add some tests for date conversions

--- test/brainbot/nozzle/esconnect_test.clj
+++ test/brainbot/nozzle/esconnect_test.clj
@@ -25,3 +25,11 @@
                  {:create-directories []}))
     (is (=submap (compare-directories dir-listing-with-error dir-listing1)
                  {:delete-directories []}))))
+
+(deftest test-date-converters
+  (testing "mtime->lastmodified"
+    (is (= "1970-01-01T00:00:00.000Z" (mtime->lastmodified 0)))
+    (is (= "2013-08-20T08:38:20.000Z" (mtime->lastmodified 1376987900))))
+  (testing "lastmodified->mtime"
+    (is (= 0 (lastmodified->mtime "1970-01-01T00:00:00.000Z")))
+    (is (= 1376987900 (lastmodified->mtime "2013-08-20T08:38:20.000Z")))))
