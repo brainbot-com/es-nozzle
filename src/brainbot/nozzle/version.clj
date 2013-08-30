7440bf88c59b628bb9f25a8e8d10340dd3d93689
Ralf Schmitt <ralf@systemexit.de>
2013-08-30 13:15:04 +0200

    make enhanced-version work with strings

--- src/brainbot/nozzle/version.clj
+++ src/brainbot/nozzle/version.clj
@@ -4,14 +4,20 @@
 
 (defn enhanced-version
   "return version string with git revision appended if it's a snapshot"
-  [group artifact]
-  (if-let [props (v/map-from-property-filepath
-                  (v/get-properties-filename group artifact))]
-    (let [version (get props "version")]
-      (if (re-find #"SNAPSHOT" version)
-        (str version "-" (subs (props "revision") 0 6))
-        version))
-    (System/getProperty (str artifact ".version"))))
+  ([group artifact]
+     (if-let [props (v/map-from-property-filepath
+                     (v/get-properties-filename group artifact))]
+       (let [version (get props "version")]
+         (if (re-find #"SNAPSHOT" version)
+           (str version "-" (subs (props "revision") 0 6))
+           version))
+       (System/getProperty (str artifact ".version"))))
+  ([s]
+     (let [[group artifact] (clojure.string/split s #"/" 2)]
+       (if artifact
+         (enhanced-version group artifact)
+         (enhanced-version group group)))))  ;; some project do that! e.g. clj-time
+
 
 
 (defn nozzle-version
