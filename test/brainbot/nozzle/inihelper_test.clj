9a2e34c0e968b6ee65101e0dbda32dd1512b5946
Ralf Schmitt <ralf@systemexit.de>
2013-09-13 18:43:22 +0200

    pass around system map instead of iniconfig in inihelper-test

--- test/brainbot/nozzle/inihelper_test.clj
+++ test/brainbot/nozzle/inihelper_test.clj
@@ -1,9 +1,12 @@
 (ns brainbot.nozzle.inihelper-test
   (:require [clojure.test :refer :all]
+            [brainbot.nozzle.sys :as sys]
             [brainbot.nozzle.inihelper :refer :all]
             [brainbot.nozzle.fsfilter :as fsfilter]))
 
 (deftest test-loading-dotfiles
-  (let [relpath-filter (dynaload-section {"dotfile" {"type" "dotfile"}} "dotfile")]
+  (let [relpath-filter (dynaload-section
+                        (sys/make-system {"dotfile" {"type" "dotfile"}} [])
+                        "dotfile")]
     (is (true? ((fsfilter/make-match-entry? relpath-filter) {:relpath ".foo"})))
     (is (false?((fsfilter/make-match-entry? relpath-filter) {:relpath "foo"})))))
