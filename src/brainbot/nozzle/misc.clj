09ed0019b146291460b7e6b161b39b37e6eab4b8
Ralf Schmitt <ralf@systemexit.de>
2013-09-19 12:28:43 +0200

    make code a bit more readable in misc.clj

--- src/brainbot/nozzle/misc.clj
+++ src/brainbot/nozzle/misc.clj
@@ -22,10 +22,11 @@
 
 
 (defn ensure-endswith-slash
-  [a-string]
-  (if (.endsWith a-string "/")
-    a-string
-    (str a-string "/")))
+  "append slash to String s if it doesn't already end with a slash"
+  [^String s]
+  (if (.endsWith s "/")
+    s
+    (str s "/")))
 
 (defn die
   "print error message msg and exit the program with named argument
@@ -38,10 +39,12 @@
 
 (defn trimmed-lines-from-string
   "split string at newline and return non-empty trimmed lines"
-  [s]
+  [^String s]
   (if s
-    (remove string/blank?
-            (map string/trim (string/split-lines s)))))
+    (->> s
+         string/split-lines
+         (map string/trim)
+         (remove string/blank?))))
 
 
 (defn setup-logging!
