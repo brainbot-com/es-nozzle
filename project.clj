48c36480c2267131a68779c8a284c45ce8e5bf8b
Ralf Schmitt <ralf@systemexit.de>
2013-09-20 13:41:59 +0200

    rename project to es-nozzle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.brainbot/nozzle "0.2.0-SNAPSHOT"
+(defproject com.brainbot/es-nozzle "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License"
