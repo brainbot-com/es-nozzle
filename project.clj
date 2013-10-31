fefb74de76ed2b0532231571665e38767b0a93a1
Ralf Schmitt <ralf@systemexit.de>
2013-10-31 16:39:21 +0100

    set version to 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.brainbot/es-nozzle "0.3.1-SNAPSHOT"
+(defproject com.brainbot/es-nozzle "0.3.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License"
