904950d55ec2f4b2efff5be1155c64217303504c
Ralf Schmitt <ralf@systemexit.de>
2013-11-24 20:35:00 +0100

    remove some unused code in async-helper

--- src/brainbot/nozzle/async_helper.clj
+++ src/brainbot/nozzle/async_helper.clj
@@ -26,44 +26,7 @@ ms milliseconds before calling f again"
        {::ctrl-ch ctrl-ch})))
 
 
-(defn looping-thread
-  "call f in a loop from a thread. sleep ms milliseconds between."
-  ([ms f]
-     (looping-thread ms f nil))
-  ([ms f dest-ch]
-     (assert (ifn? f) "f must be a function")
-     (looping-go ms #(async/thread-call f))))
-
-
 (defn looping-stop
   "stop function calling loop"
   [{ctrl-ch ::ctrl-ch}]
   (async/close! ctrl-ch))
-
-
-
-(defn make-looping-thread-mult
-  "call f in a loop from a thread. sleep ms milliseconds between.
-put the result of calling f into a core.async mult. the mult is
-returned inside the return value as :mult key
-"
-  [ms f]
-  (assert (ifn? f) "f must be a function")
-  (let [ch (chan)
-        m (mult ch)]
-    (assoc (looping-thread ms f ch)
-      :mult m)))
-
-#_(defn mark
-  []
-  (go
-   (let [c (System/currentTimeMillis)]
-     (println "hello" c)
-     c)))
-
-;; (def m (make-looping-thread-mult 2000 #(let [c (System/currentTimeMillis)] (println "hello" c) c)))
-;; (def ch (chan))
-
-
-
-
