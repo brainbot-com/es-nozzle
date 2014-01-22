f8f98ff028a8834f0e8c274d354f84ab944b2007
Ralf Schmitt <ralf@systemexit.de>
2014-01-22 10:30:07 +0100

    update changelog, up version to 0.4.0-SNAPSHOT
    I think thumbnail generation justifies a new minor release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.brainbot/es-nozzle "0.3.2-SNAPSHOT"
+(defproject com.brainbot/es-nozzle "0.4.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License"
